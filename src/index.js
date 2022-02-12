b80df57acde855b3d5b779fb95c9816c33d68ff2
Caio Simões <caiossfer2@gmail.com>
2022-02-12 00:12:37 -0300

    primeiro

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css';
+
+
+function Nav(props){
+    return(
+        <nav className="nav-text">
+            <h1>{props.texto.toUpperCase()}</h1>
+        </nav>  
+    );
+}
+
+function Linha(props){
+
+    function trocarIcone(){
+        console.log("oi");
+    }
+
+
+    return(
+       <div className="linha">   
+            <img onClick = {trocarIcone} className="icon" id="empty" src="square-icon.png" alt='itens não concluidos' /> 
+            <img className="icon" id="check" src="checked-icon.png" alt='itens não concluidos'/> 
+            <p>{props.texto}</p>
+       </div>
+    );
+}
+
+function MainSection(props){
+    return (
+        <section className="main-section">
+            <div className="linhas">
+                <Linha texto="Fazer mochup" /> 
+                <Linha texto="Criar página estática" /> 
+                <Linha texto="Aplicar state" /> 
+                <Linha texto="Conectar Eventos" /> 
+            </div>
+        </section>
+    );
+}
+
+function App(){
+    return(
+        <div>
+            <Nav texto="to-do list"/>
+            <MainSection/>
+        </div>
+    );
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
